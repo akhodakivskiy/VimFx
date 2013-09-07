akhodakivskiy/VimFx

commit 586dd356b922bc4fea06d94eef51991bef0de701

Performance fix for markers injection

extension/packages/hints.coffee
@@ -51,25 +51,43 @@ removeHints = (document) ->
 
 # Like `injectMarkers`, but also sets hints for the markers
 injectHints = (document) ->
-  markers = injectMarkers(document)
+  markers = createMarkers(document)
   hintChars = utils.getHintChars()
 
   addHuffmanCodeWordsTo(markers, {alphabet: hintChars}, (marker, hint) -> marker.setHint(hint))
 
+  removeHints(document)
+  insertHints(markers)
+
   return markers
+
+insertHints = (markers) ->
+  docFrags = []
+
+  getFrag = (document) ->
+    for [doc, frag] in docFrags
+      if document == doc
+        return frag
+
+  for marker in markers
+    doc = marker.element.ownerDocument
+    if not getFrag(doc)
+      docFrags.push([doc, doc.createDocumentFragment()])
+      
+    frag = getFrag(doc)
+    frag.appendChild(marker.markerElement)
+
+  for [doc, frag] in docFrags
+    container = createHintsContainer(doc)
+    container.appendChild(frag)
+    doc.documentElement.appendChild(container)
 
 
 # Creates and injects markers into the DOM
-injectMarkers = (document) ->
-  # First remove previous hints container
-  removeHints(document)
-
+createMarkers = (document) ->
   # For now we aren't able to handle hint markers in XUL Documents :(
   if document instanceof HTMLDocument# or document instanceof XULDocument
     if document.documentElement
-      # For performance use Document Fragment
-      fragment = document.createDocumentFragment()
-
       # Select all markable elements in the document, create markers
       # for each of them, and position them on the page.
       # Note that the markers are not given hints.
@@ -79,20 +97,13 @@ injectMarkers = (document) ->
         element = set.snapshotItem(i)
         if rect = getElementRect(element)
           marker = new Marker(element)
-
           marker.setPosition(rect.top, rect.left)
-          fragment.appendChild(marker.markerElement)
-
           marker.weight = rect.area * marker.calcBloomRating()
 
           markers.push(marker)
 
-      container = createHintsContainer(document)
-      container.appendChild(fragment)
-      document.documentElement.appendChild(container)
-
       for frame in document.defaultView.frames
-        markers = markers.concat(injectMarkers(frame.document))
+        markers = markers.concat(createMarkers(frame.document))
 
   return markers or []
 

commit 00b5d9164199cdaade5d4bb7e909e68a5706b8a0

Fix: commit 586dd356 broke hint marker rotation

extension/packages/hints.coffee
@@ -49,7 +49,7 @@ removeHints = (document) ->
     removeHints(frame.document)
 
 
-# Like `injectMarkers`, but also sets hints for the markers
+# Like `insertHints`, but also sets hints for the markers
 injectHints = (document) ->
   markers = createMarkers(document)
   hintChars = utils.getHintChars()
@@ -58,6 +58,10 @@ injectHints = (document) ->
 
   removeHints(document)
   insertHints(markers)
+
+  # Must be done after the hints have been inserted into the DOM (see marker.coffee)
+  for marker in markers
+    marker.completePosition()
 
   return markers
 
@@ -73,7 +77,7 @@ insertHints = (markers) ->
     doc = marker.element.ownerDocument
     if not getFrag(doc)
       docFrags.push([doc, doc.createDocumentFragment()])
-      
+
     frag = getFrag(doc)
     frag.appendChild(marker.markerElement)
 

extension/packages/marker.coffee
@@ -70,8 +70,6 @@ class Marker
 
     @markerElement.appendChild(fragment)
 
-    @completePosition()
-
   # Add another char to the `enteredHintString`,
   # see if it still matches `hintString`, apply classes to
   # the distinct hint characters and show/hide marker when