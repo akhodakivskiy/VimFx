e9d51a7f53a225957e4ec12e987e4a2afa629bce
Anton Khodakivskiy <akhodakivskiy@gmail.com>
2014-01-11 16:44:12 -0800

    Added a few mode arrows to the pagination patterns. Closes #254

--- extension/defaults/preferences/defaults.js
+++ extension/defaults/preferences/defaults.js
@@ -1,6 +1,6 @@
 pref('extensions.VimFx.hint_chars',         'fjdkslaghrueiwovncm');
-pref('extensions.VimFx.prev_patterns',      'prev,previous,back,older,<,«,<<');
-pref('extensions.VimFx.next_patterns',      'next,more,newer,>,»,>>');
+pref('extensions.VimFx.prev_patterns',      'prev,previous,back,older,<,«,<<,◀,←');
+pref('extensions.VimFx.next_patterns',      'next,more,newer,>,»,>>,▶,→');
 pref('extensions.VimFx.scroll_step_lines',  6);
 pref('extensions.VimFx.black_list',         '*example.com*');
 pref('extensions.VimFx.hints_bloom_data',   '');
