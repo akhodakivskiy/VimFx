akhodakivskiy/VimFx

commit 33ed14df3a16c91594d340383e5410b864837a25

Stop URI encoding the API URL pref
The reason we used to was to make it easier to consume the public API (because
it is easier to read an ASCII string pref than a unicode one). However, some
systems return spaces already URI encoded, which breaks when encoding it once
more. In the end, this tiny simplification of the API consumption is not worth
it. Better to simply store the API URL as-is in a complex string pref.

Fixes #516.

extension/lib/main.coffee
@@ -44,14 +44,12 @@ module.exports = (data, reason) ->
   vimfx.version = data.version
   AddonManager.getAddonByID(vimfx.id, (info) -> vimfx.info = info)
 
-  # Setup the public API. The URL is encoded (which means that there will be
-  # only ASCII characters) so that the pref can be read using a simple
-  # `Services.prefs.getCharPref()` (which only supports ASCII characters).
-  api_url = encodeURI("#{ data.resourceURI.spec }lib/public.js")
-  { setAPI } = Cu.import(api_url, {})
+  # Setup the public API.
+  apiUrl = "#{ data.resourceURI.spec }lib/public.js"
+  { setAPI } = Cu.import(apiUrl, {})
   setAPI(createAPI(vimfx))
-  module.onShutdown(-> Cu.unload(api_url))
-  prefs.set('api_url', api_url)
+  module.onShutdown(-> Cu.unload(apiUrl))
+  prefs.set('apiUrl', apiUrl)
 
   test?(vimfx)
 

commit d4a03bdd35d7ea1c140170ac5ae25aa5613745d4

Fix broken setting of the API URL pref
Accidentally broken in commit 33ed14df. I hadn't noticed before, since the
correct pref still remained in my profiles. Testing in a new profile revealed
the error.

extension/lib/main.coffee
@@ -50,7 +50,7 @@ module.exports = (data, reason) ->
   { setAPI, removeAPI } = Cu.import(apiUrl, {})
   setAPI(createAPI(vimfx))
   module.onShutdown(removeAPI)
-  prefs.set('apiUrl', apiUrl)
+  prefs.set('api_url', apiUrl)
 
   utils.loadCss('style')
 