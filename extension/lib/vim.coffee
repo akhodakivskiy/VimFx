akhodakivskiy/VimFx

commit b50f5ea597b1425ebaa0d38784589c4fe0fddc41

Properly handle key events in non-multi-process
This commit can alternatively be lableled:

"Fix `Vim::isFrameEvent`" in in-multi-process.

Before, _no key events were considered frame events non-multi-process, because
the check didn't work there. Most notably, this caused `<escape>` to be leaked
to the page when blurring text inputs.

extension/lib/vim.coffee
@@ -71,8 +71,12 @@ class Vim
   _isBlacklisted: (url) -> @options.black_list.some((regex) -> regex.test(url))
 
   isFrameEvent: (event) ->
-    return (@_state.frameCanReceiveEvents and
-            event.originalTarget == @window.gBrowser.selectedBrowser)
+    target = event.originalTarget
+    return @_state.frameCanReceiveEvents and
+      if MULTI_PROCESS_ENABLED
+        (target == @window.gBrowser.selectedBrowser)
+      else
+        not (target.ownerGlobal == @window)
 
   isCurrent: ->
     return (@_parent.getCurrentVim(utils.getCurrentWindow()) == this and

commit facbda60066451c24f14ffaf898e2ea309b58840

Fix commands not working on slowly-loading pages
This was broken in commit b50f5ea5.

See #588.

extension/lib/vim.coffee
@@ -82,7 +82,7 @@ class Vim
 
   isUIEvent: (event) ->
     target = event.originalTarget
-    return @_state.frameCanReceiveEvents and
+    return not @_state.frameCanReceiveEvents or
       if MULTI_PROCESS_ENABLED
         (target != @window.gBrowser.selectedBrowser)
       else