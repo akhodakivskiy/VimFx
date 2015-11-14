akhodakivskiy/VimFx

commit e552942ccd8a00fd8c5906c819ef51207f65719e

Allow to use numbers as shortcuts, overriding counts
... properly handling the special-case for `0`:

- `0J` is _not_ a count, because a count of zero makes no sense. It first
  invokes the `0` command, and then the `J` command.
- `20J` _is_ a count, repeating twenty times. In other words, pressing `2` and
  then `0` does _not_ run the `0` command (scroll to the far left) with a count
  of two. Instead, it sets the waiting count to twenty.

Fixes #587.

extension/lib/vimfx.coffee
@@ -87,12 +87,8 @@ class VimFx extends utils.EventEmitter
     command = null
 
     switch
-      when toplevel and DIGIT.test(keyStr) and
-           not (keyStr == '0' and @count == '')
-        @count += keyStr
-        type = 'count'
-
-      when keyStr of @currentKeyTree
+      when keyStr of @currentKeyTree and
+           not (keyStr == '0' and @count != '')
         next = @currentKeyTree[keyStr]
         if next instanceof Leaf
           type = 'full'
@@ -100,6 +96,11 @@ class VimFx extends utils.EventEmitter
         else
           @currentKeyTree = next
           type = 'partial'
+
+      when toplevel and DIGIT.test(keyStr) and
+           not (keyStr == '0' and @count == '')
+        @count += keyStr
+        type = 'count'
 
       else
         @reset(mode)

commit 3387781421dbfbe588e1d4c4e918d2255e4f6483

Fix problem with counts and shortcut including `0`
For instance, `5g0` didn't trigger the `g0` command. This was broken in commit
e552942c.

extension/lib/vimfx.coffee
@@ -88,7 +88,7 @@ class VimFx extends utils.EventEmitter
 
     switch
       when keyStr of @currentKeyTree and
-           not (keyStr == '0' and @count != '')
+           not (toplevel and keyStr == '0' and @count != '')
         next = @currentKeyTree[keyStr]
         if next instanceof Leaf
           type = 'full'