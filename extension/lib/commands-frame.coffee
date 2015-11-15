akhodakivskiy/VimFx

commit 7b69d5f69cc54f3cd8c2425c183fe8ebad6ac87f

Make Facebook's comment fields focusable using `f`
Fixes #524.

extension/lib/commands-frame.coffee
@@ -114,6 +114,8 @@ commands.follow = ({ vim, storage }) ->
            element.hasAttribute('onmouseup') or
            element.hasAttribute('oncommand') or
            element.getAttribute('role') in ['link', 'button'] or
+           # Facebook special-case (comment fields).
+           element.parentElement?.classList.contains('UFIInputContainer') or
            # Twitter special-case.
            element.classList.contains('js-new-tweets-bar') or
            # Feedly special-case.

extension/lib/utils.coffee
@@ -191,6 +191,7 @@ suppressEvent = (event) ->
 eventSequence = ['mouseover', 'mousedown', 'mouseup', 'click', 'command']
 simulateClick = (element) ->
   window = element.ownerDocument.defaultView
+  element.click() # Needed for Facebook’s comment fields.
   for type in eventSequence
     mouseEvent = new window.MouseEvent(type, {
       # Let the event bubble in order to trigger delegated event listeners.

commit 3041abba974d8e10ec633f20798a3405a5ff1921

Fix nothing happening when clicking some buttons
... using `f`. This was broken by commit 7b69d5f6.

extension/lib/commands-frame.coffee
@@ -114,8 +114,6 @@ commands.follow = ({ vim, storage }) ->
            element.hasAttribute('onmouseup') or
            element.hasAttribute('oncommand') or
            element.getAttribute('role') in ['link', 'button'] or
-           # Facebook special-case (comment fields).
-           element.parentElement?.classList.contains('UFIInputContainer') or
            # Twitter special-case.
            element.classList.contains('js-new-tweets-bar') or
            # Feedly special-case.
@@ -124,6 +122,9 @@ commands.follow = ({ vim, storage }) ->
            element.hasAttribute('data-page-action')
         type = 'clickable'
         semantic = false
+      # Facebook special-case (comment fields).
+      when element.parentElement?.classList.contains('UFIInputContainer')
+        type = 'clickable-special'
       # Putting markers on `<label>` elements is generally redundant, because
       # its `<input>` gets one. However, some sites hide the actual `<input>`
       # but keeps the `<label>` to click, either for styling purposes or to keep
@@ -213,12 +214,15 @@ commands.focus_marker_element = ({ storage, elementIndex, options }) ->
   utils.focusElement(element, options)
 
 commands.click_marker_element = (args) ->
-  { vim, storage, elementIndex, preventTargetBlank } = args
+  { vim, storage, elementIndex, preventTargetBlank, type } = args
   element = storage.markerElements[elementIndex]
   if element.target == '_blank' and preventTargetBlank
     targetReset = element.target
     element.target = ''
-  utils.simulateClick(element)
+  if type == 'clickable-special'
+    element.click()
+  else
+    utils.simulateClick(element)
   element.target = targetReset if targetReset
 
 commands.copy_marker_element = ({ storage, elementIndex, property }) ->

extension/lib/commands.coffee
@@ -323,6 +323,7 @@ helper_follow_clickable = ({ inTab, inBackground }, { vim, count = 1 }) ->
       vim._run('click_marker_element', {
         elementIndex
         preventTargetBlank: vim.options.prevent_target_blank
+        type: marker.type
       })
 
     return not isLast

extension/lib/utils.coffee
@@ -191,7 +191,6 @@ suppressEvent = (event) ->
 eventSequence = ['mouseover', 'mousedown', 'mouseup', 'click', 'command']
 simulateClick = (element) ->
   window = element.ownerDocument.defaultView
-  element.click() # Needed for Facebook’s comment fields.
   for type in eventSequence
     mouseEvent = new window.MouseEvent(type, {
       # Let the event bubble in order to trigger delegated event listeners.