akhodakivskiy/VimFx

commit 7b187a90ad3c9b5453930c5c9835301891f9dbf2

Don't consider `<select>`s as text inputs for `gi`

extension/lib/utils.coffee
@@ -79,11 +79,14 @@ isTextInputElement = (element) ->
            'text', 'search', 'tel', 'url', 'email', 'password', 'number'
          ]) or
          element instanceof HTMLTextAreaElement or
+         element instanceof XULTextBoxElement
+
+isTypingElement = (element) ->
+  return isTextInputElement(element) or
          # `<select>` elements can also receive text input: You may type the
          # text of an item to select it.
          element instanceof HTMLSelectElement or
-         element instanceof XULMenuListElement or
-         element instanceof XULTextBoxElement
+         element instanceof XULMenuListElement
 
 
 
@@ -319,6 +322,7 @@ module.exports = {
   isContentEditable
   isProperLink
   isTextInputElement
+  isTypingElement
 
   getActiveElement
   blurActiveElement

extension/lib/commands-frame.coffee
@@ -25,7 +25,7 @@
 hints = require('./hints')
 utils = require('./utils')
 
-{ isProperLink, isTextInputElement, isContentEditable } = utils
+{ isProperLink, isTextInputElement, isTypingElement, isContentEditable } = utils
 
 XULDocument = Ci.nsIDOMXULDocument
 
@@ -94,7 +94,7 @@ commands.follow = ({ vim, storage }) ->
     switch
       when isProperLink(element)
         type = 'link'
-      when isTextInputElement(element) or isContentEditable(element)
+      when isTypingElement(element) or isContentEditable(element)
         type = 'text'
       when element.tabIndex > -1 and
            not (isXUL and element.nodeName.endsWith('box') and
@@ -173,9 +173,9 @@ commands.follow_copy = ({ vim, storage }) ->
   storage.markerElements = []
   filter = (element, getElementShape) ->
     type = switch
-      when isProperLink(element)       then 'link'
-      when isTextInputElement(element) then 'textInput'
-      when isContentEditable(element)  then 'contenteditable'
+      when isProperLink(element)      then 'link'
+      when isTypingElement(element)   then 'typing'
+      when isContentEditable(element) then 'contenteditable'
     return unless type
     return unless shape = getElementShape(element)
     length = storage.markerElements.push(element)
@@ -263,7 +263,7 @@ commands.focus_text_input = ({ vim, storage, count = null }) ->
   { lastFocusedTextInput } = vim.state
   inputs = Array.filter(
     vim.content.document.querySelectorAll('input, textarea'), (element) ->
-      return utils.isTextInputElement(element) and utils.area(element) > 0
+      return isTextInputElement(element) and utils.area(element) > 0
   )
   if lastFocusedTextInput and lastFocusedTextInput not in inputs
     inputs.push(lastFocusedTextInput)

extension/lib/commands.coffee
@@ -353,7 +353,7 @@ commands.follow_copy = ({ vim }) ->
     { elementIndex } = marker.wrapper
     property = switch marker.wrapper.type
       when 'link'            then 'href'
-      when 'textInput'       then 'value'
+      when 'typing'          then 'value'
       when 'contenteditable' then 'textContent'
     vim._run('copy_marker_element', {elementIndex, property})
   helper_follow('follow_copy', vim, callback)

extension/lib/events-frame.coffee
@@ -146,7 +146,7 @@ class FrameEventManager
           # …and the event is programmatic (not caused by clicks or keypresses)…
           focusManager.getLastFocusMethod(null) == 0 and
           # …and the target may steal most keystrokes.
-          (utils.isTextInputElement(target) or utils.isContentEditable(target))
+          (utils.isTypingElement(target) or utils.isContentEditable(target))
         # Some sites (such as icloud.com) re-focuses inputs if they are blurred,
         # causing an infinite loop of autofocus prevention and re-focusing.
         # Therefore, blur events that happen just after an autofocus prevention
@@ -165,7 +165,7 @@ class FrameEventManager
       # will, and should, be re-focused (because it was focused when you left
       # the tab). This case is kept track of so that the autofocus prevention
       # does not catch it.
-      if utils.isTextInputElement(target) or utils.isContentEditable(target)
+      if utils.isTypingElement(target) or utils.isContentEditable(target)
         utils.nextTick(@vim.content, =>
           @vim.state.shouldRefocus = not @vim.content.document.hasFocus()
         )

commit 46b8f601b97ef421c0e1bd05383df33960166fbe

Fix not being able to type in `<select>`s
Typing in a `<select>` should select the matching `<option>`. Regression since
commit 7b187a90.

extension/lib/utils.coffee
@@ -146,7 +146,7 @@ moveFocus = (direction) ->
 getFocusType = (event) ->
   target = event.originalTarget
   return switch
-    when isTextInputElement(target) or isContentEditable(target)
+    when isTypingElement(target) or isContentEditable(target)
       'editable'
     when isActivatable(target)
       'activatable'