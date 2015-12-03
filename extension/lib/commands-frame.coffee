akhodakivskiy/VimFx

commit b3e8a037eb347d3c196fe975594dd161b8cd2d0d

Fix text selection when forusing text inputs
When using one of the `f` commands to focus a text input, the previous selection
or caret position should be retained, just as if you had clicked it. However,
before this commit all of their text was accidentally selected (as if you had
pressed `<tab>` to foucs).

This also improves the `gi` command. Previously, it _always_ selected all text.
Now it does so only if you use a count, or use `gi` to re-focus an input after a
prevented autofocus. In those cases you likely want to replace what's in the
input, but otherwise you're likely returning to the text input after a temporary
leave and want to continue typing where you left off.

extension/lib/commands-frame.coffee
@@ -286,14 +286,16 @@ commands.focus_text_input = ({vim, count = null}) ->
     inputs.push(lastFocusedTextInput)
   return unless inputs.length > 0
   inputs.sort((a, b) -> a.tabIndex - b.tabIndex)
-  unless count?
-    count =
-      if lastFocusedTextInput
-        inputs.indexOf(lastFocusedTextInput) + 1
-      else
-        1
-  index = Math.min(count, inputs.length) - 1
-  utils.focusElement(inputs[index], {select: true})
+  num = switch
+    when count?
+      count
+    when lastFocusedTextInput
+      inputs.indexOf(lastFocusedTextInput) + 1
+    else
+      1
+  index = Math.min(num, inputs.length) - 1
+  select = (count? or vim.state.autofocusPrevented)
+  utils.focusElement(inputs[index], {select})
   vim.state.inputs = inputs
 
 commands.clear_inputs = ({vim}) ->

extension/lib/utils.coffee
@@ -125,11 +125,13 @@ blurActiveBrowserElement = (vim) ->
   )
 
 # Focus an element and tell Firefox that the focus happened because of a user
-# keypress (not just because some random programmatic focus).
+# action (not just because some random programmatic focus). `.FLAG_BYKEY` might
+# look more appropriate, but it unconditionally selects all text, which
+# `.FLAG_BYMOUSE` does not.
 focusElement = (element, options = {}) ->
   focusManager = Cc['@mozilla.org/focus-manager;1']
     .getService(Ci.nsIFocusManager)
-  focusManager.setFocus(element, focusManager.FLAG_BYKEY)
+  focusManager.setFocus(element, focusManager.FLAG_BYMOUSE)
   element.select?() if options.select
 
 getFocusType = (element) -> switch

extension/lib/events-frame.coffee
@@ -189,6 +189,7 @@ class FrameEventManager
         @numFocusToSuppress--
         return
 
+      @vim.state.autofocusPrevented = false
       sendFocusType()
 
       # Reset `hasInteraction` when (re-)selecting a tab, or coming back from
@@ -233,6 +234,7 @@ class FrameEventManager
         # are suppressed.
         @listenOnce('blur', utils.suppressEvent)
         target.blur()
+        @vim.state.autofocusPrevented = true
     )
 
     @listen('blur', (event) =>

extension/lib/vim-frame.coffee
@@ -52,6 +52,7 @@ class VimFrame
         hasInteraction:       false
         shouldRefocus:        false
         marks:                {}
+        autofocusPrevented:   false
         lastFocusedTextInput: null
         scrollableElements:   new ScrollableElements(@content, MINIMUM_SCROLL)
         markerElements:       []

commit 38d984c58004ef4a1220f76c5ebdae881085121f

Fix not being able to focus scrollable elements
Regression since commit b3e8a037e.

extension/lib/commands-frame.coffee
@@ -216,6 +216,8 @@ commands.follow_focus = helper_follow.bind(null, {id: 'focus', combine: false},
 
 commands.focus_marker_element = ({vim, elementIndex, options}) ->
   element = vim.state.markerElements[elementIndex]
+  # To be able to focus scrollable elements, `FLAG_BYKEY` _has_ to be used.
+  options.flag = 'FLAG_BYKEY' if vim.state.scrollableElements.has(element)
   utils.focusElement(element, options)
 
 commands.click_marker_element = (args) ->

extension/lib/utils.coffee
@@ -131,7 +131,7 @@ blurActiveBrowserElement = (vim) ->
 focusElement = (element, options = {}) ->
   focusManager = Cc['@mozilla.org/focus-manager;1']
     .getService(Ci.nsIFocusManager)
-  focusManager.setFocus(element, focusManager.FLAG_BYMOUSE)
+  focusManager.setFocus(element, options.flag ? 'FLAG_BYMOUSE')
   element.select?() if options.select
 
 getFocusType = (element) -> switch