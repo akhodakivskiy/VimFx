akhodakivskiy/VimFx

commit cc80c0524c28775809566ec7afa46a153853db9d

Add command to click browser elements using markers
Fixes #236.

A note about the "hamburger menu" in the top-right corner:

It is possible to _open_ it with `zF`, but not possible to use `zF` inside of
it, because of `@popupPassthrough`. One _could_ an exception for that
menu/popup, but that doesn't help:

- `.elementFromPoint()` does not appear to find elements in popups, making it
  difficult to determine if the buttons inside are visible or not.
- The markers appear _behind_ the popup, no matter what `z-index` is used.
- The popup seems to eat `<escape>`: When it is pressed, the popup is closed,
  but the markers remain. The next `<escape>` press removes the markers. Not a
  big deal, but slightly annoying.

Therefore I decided not to add support for that menu in this commit. (Perhaps
we'll be able to do that some time in the future.) Instead, I recommend using
the alt key shortcuts for the "regular" menubar.

extension/lib/hints.coffee
@@ -107,15 +107,19 @@ injectHints = (window, wrappers, viewport, options) ->
   removeHints(window) # Better safe than sorry.
   container = window.document.createElement('box')
   container.id = CONTAINER_ID
-  window.gBrowser.mCurrentBrowser.parentNode.appendChild(container)
 
-  zoom =
+  zoom = 1
+
+  if options.ui
+    container.classList.add('ui')
+    window.document.getElementById('browser-panel').appendChild(container)
+  else
+    window.gBrowser.mCurrentBrowser.parentNode.appendChild(container)
+    # If “full zoom” is not used, it means that “Zoom text only” is enabled.
+    # If so, that “zoom” does not need to be taken into account.
     if window.ZoomManager.useFullZoom
-      window.ZoomManager.getZoomForBrowser(window.gBrowser.selectedBrowser)
-    else
-      # If “full zoom” is not used, it means that “Zoom text only” is enabled.
-      # If so, that “zoom” does not need to be taken into account.
-      1
+      zoom =
+        window.ZoomManager.getZoomForBrowser(window.gBrowser.selectedBrowser)
 
   for marker in markers
     container.appendChild(marker.markerElement)
@@ -171,7 +175,7 @@ getMarkableElements = (window, viewport, wrappers, filter, parents = []) ->
     )
     wrappers.push(wrapper)
 
-  for frame in window.frames
+  for frame in window.frames when frame.frameElement
     rect = frame.frameElement.getBoundingClientRect() # Frames only have one.
     continue unless isInsideViewport(rect, viewport)
 
@@ -203,20 +207,24 @@ getAllElements = (document) ->
   unless document instanceof XULDocument
     return document.getElementsByTagName('*')
 
-  elements = []
+  # Use a `Set` since this algorithm may find the same element more than once.
+  # Ideally we should find a way to find all elements without duplicates.
+  elements = new Set()
   getAllRegular = (element) ->
-    for child in element.getElementsByTagName('*')
-      elements.push(child)
+    # The first time `zF` is run `.getElementsByTagName('*')` may oddly include
+    # `undefined` in its result! Filter those out.
+    for child in element.getElementsByTagName('*') when child
+      elements.add(child)
       getAllAnonymous(child)
     return
   getAllAnonymous = (element) ->
     for child in document.getAnonymousNodes(element) or []
       continue unless child instanceof Element
-      elements.push(child)
+      elements.add(child)
       getAllRegular(child)
     return
   getAllRegular(document.documentElement)
-  return elements
+  return Array.from(elements)
 
 getRects = (element, viewport) ->
   # `element.getClientRects()` returns a list of rectangles, usually just one,
@@ -327,10 +335,8 @@ getFirstNonCoveredPoint = (window, viewport, element, elementRect, parents) ->
     # bullet proof: Combinations of CSS can cause this check to fail, even
     # though `element` isn’t covered. We don’t try to temporarily reset such CSS
     # because of performance. Instead we rely on that some of the attempts below
-    # will work.
-    if element.contains(elementAtPoint) or # Note that `a.contains(a) == true`!
-       (window.document instanceof XULDocument and
-        getClosestNonAnonymousParent(element) == elementAtPoint)
+    # will work. Note that `a.contains(a) == true`!
+    if normalize(element).contains(elementAtPoint)
       found = true
       # If we’re currently in a frame, there might be something on top of the
       # frame that covers `element`. Therefore we ensure that the frame really
@@ -342,7 +348,7 @@ getFirstNonCoveredPoint = (window, viewport, element, elementRect, parents) ->
         elementAtPoint = parent.window.document.elementFromPoint(
           offset.left + x + dx, offset.top + y + dy
         )
-        unless elementAtPoint == currentWindow.frameElement
+        unless frameAtPoint(currentWindow.frameElement, elementAtPoint)
           found = false
           break
         currentWindow = parent.window
@@ -381,12 +387,24 @@ getFirstNonCoveredPoint = (window, viewport, element, elementRect, parents) ->
 
   return nonCoveredPoint
 
-# In XUL documents there are “anonymous” elements, whose node names start with
-# `xul:` or `html:`. These are never returned by `document.elementFromPoint` but
-# their closest non-anonymous parents are.
-getClosestNonAnonymousParent = (element) ->
-  element = element.parentNode while element.prefix?
-  return element
+# In XUL documents there are “anonymous” elements. These are never returned by
+# `document.elementFromPoint` but their closest non-anonymous parents are.
+normalize = (element) ->
+  return element.ownerDocument.getBindingParent(element) or element
+
+# Returns whether `frameElement` corresponds to `elementAtPoint`. This is only
+# complicated for the dev tools’ frame. `.elementAtPoint()` returns
+# `<tabbrowser#content>` instead of the `<iframe>`. The dev tools might be in
+# another tab and thus invisible, but `<tabbrowser#content>` is the same and
+# visible in _all_ tabs, so we have to check that the frame really belongs to
+# the current tab.
+frameAtPoint = (frameElement, elementAtPoint) ->
+  frame = normalize(frameElement)
+  return false unless elementAtPoint == frame
+  return true  unless frame.nodeName == 'tabbrowser' and frame.id == 'content'
+  {gBrowser} = frameElement.ownerGlobal.top
+  tabpanel = gBrowser.getNotificationBox(gBrowser.selectedBrowser)
+  return tabpanel.contains(frameElement)
 
 module.exports = {
   removeHints

extension/lib/commands.coffee
@@ -429,6 +429,37 @@ commands.follow_focus = ({vim}) ->
     vim._focusMarkerElement(marker.wrapper.elementIndex, {select: true})
   return helper_follow('follow_focus', vim, callback)
 
+commands.click_browser_element = ({vim}) ->
+  markerElements = []
+
+  filter = (element, getElementShape) ->
+    document = element.ownerDocument
+    unless element.tabIndex > -1 and
+           not (element.nodeName.endsWith('box') and
+                element.nodeName != 'checkbox') and
+           element.nodeName != 'tabs'
+      return
+    return unless shape = getElementShape(element)
+    length = markerElements.push(element)
+    return {type: 'clickable', semantic: true, shape, elementIndex: length - 1}
+
+  callback = (marker) ->
+    element = markerElements[marker.wrapper.elementIndex]
+    utils.focusElement(element)
+    utils.simulateClick(element)
+
+  {wrappers, viewport} =
+    hints.getMarkableElementsAndViewport(vim.window, filter)
+
+  if wrappers.length > 0
+    markers = hints.injectHints(vim.window, wrappers, viewport, {
+      hint_chars: vim.options.hint_chars
+      ui: true
+    })
+    vim.enterMode('hints', markers, callback)
+  else
+    vim.notify(translate('notification.follow.none'))
+
 helper_follow_pattern = (type, {vim}) ->
   options =
     pattern_selector: vim.options.pattern_selector

extension/lib/defaults.coffee
@@ -82,6 +82,7 @@ shortcuts =
       'af':        'follow_multiple'
       'yf':        'follow_copy'
       'zf':        'follow_focus'
+      'zF':        'click_browser_element'
       '[':         'follow_previous'
       ']':         'follow_next'
       'gi':        'focus_text_input'

extension/locale/de/vimfx.properties
@@ -67,6 +67,7 @@ mode.normal.follow_in_window=Einen Link in neuem Fenster öffnen
 mode.normal.follow_multiple=Mehreren Links in neuen Tabs folgen, Texteingabe fokussieren oder mehrere Schaltflächen betätigen
 mode.normal.follow_copy=Links oder Text-Eingabewerte in Zwischenablage kopieren
 mode.normal.follow_focus=Element fokussieren oder auswählen
+mode.normal.click_browser_element=Click browser element
 mode.normal.follow_previous=Zur vorherigen Seite springen
 mode.normal.follow_next=Zur nächsten Seite springen
 mode.normal.focus_text_input=Zuletzt fokussierte oder erste Texteingabe fokussieren

extension/locale/en-US/vimfx.properties
@@ -67,6 +67,7 @@ mode.normal.follow_in_window=Follow link in a new window
 mode.normal.follow_multiple=Follow multiple links in new background tabs, focus text input or click multiple buttons
 mode.normal.follow_copy=Copy link or text input value
 mode.normal.follow_focus=Focus/select element
+mode.normal.click_browser_element=Click browser element
 mode.normal.follow_previous=Go to the previous page
 mode.normal.follow_next=Go to the next page
 mode.normal.focus_text_input=Focus last focused or first text input

extension/locale/fr/vimfx.properties
@@ -67,6 +67,7 @@ mode.normal.follow_in_window=Follow link in a new window
 mode.normal.follow_multiple=Ouvrir plusieurs liens dans des onglets en arrière-plan
 mode.normal.follow_copy=Copier l'adresse du lien ou le texte dans le presse-papier
 mode.normal.follow_focus=Focaliser l'élément
+mode.normal.click_browser_element=Click browser element
 mode.normal.follow_previous=Aller à la page précédente
 mode.normal.follow_next=Aller à la page suivante
 mode.normal.focus_text_input=Focaliser la dernière entrée de texte à avoir été focalisé ou la première

extension/locale/id/vimfx.properties
@@ -67,6 +67,7 @@ mode.normal.follow_in_window=Ikuti link pada jendela baru
 mode.normal.follow_multiple=Ikuti banyak link pada tab latar belakang baru, fokus masukan teks atau klik banyak tombol
 mode.normal.follow_copy=Salin link atau isi masukan teks
 mode.normal.follow_focus=Fokus/pilih element
+mode.normal.click_browser_element=Click browser element
 mode.normal.follow_previous=Menuju halaman sebelum
 mode.normal.follow_next=Menuju halaman berikut
 mode.normal.focus_text_input=Fokus pada fokus terakhir atau masukan teks pertama

extension/locale/it/vimfx.properties
@@ -67,6 +67,7 @@ mode.normal.follow_in_window=Follow link in a new window
 mode.normal.follow_multiple=Apri diversi link in nuove schede non in primo piano, seleziona una casella di input di testo o clicca diversi bottoni.
 mode.normal.follow_copy=Copia il link o il testo nella casella negli appunti
 mode.normal.follow_focus=Attiva/seleziona elemento
+mode.normal.click_browser_element=Click browser element
 mode.normal.follow_previous=Vai alla pagina precedente
 mode.normal.follow_next=Vai alla prossima pagina
 mode.normal.focus_text_input=Seleziona l'ultimo input selezionato o il primo

commit 594d058e844ddf197a33cb6325a46fa3593a6f9a

Fix missing hints in the Add-ons Manager
Regression since commit cc80c0524.

extension/lib/hints.coffee
@@ -390,7 +390,9 @@ getFirstNonCoveredPoint = (window, viewport, element, elementRect, parents) ->
 # In XUL documents there are “anonymous” elements. These are never returned by
 # `document.elementFromPoint` but their closest non-anonymous parents are.
 normalize = (element) ->
-  return element.ownerDocument.getBindingParent(element) or element
+  normalized = element.ownerDocument.getBindingParent(element) or element
+  normalized = normalized.parentNode while normalized.prefix?
+  return normalized
 
 # Returns whether `frameElement` corresponds to `elementAtPoint`. This is only
 # complicated for the dev tools’ frame. `.elementAtPoint()` returns