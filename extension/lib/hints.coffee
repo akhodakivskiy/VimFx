akhodakivskiy/VimFx

commit 1176cde8c0937e0ccec8d557ee6529d5bab77d93

Enable the `no_plusplus` CoffeeLint rule

extension/lib/hints.coffee
@@ -73,7 +73,8 @@ injectHints = (window, wrappers, viewport, options) ->
   setZIndexes = (markers) ->
     markers.sort((a, b) -> a.weight - b.weight)
     for marker in markers when marker not instanceof huffman.BranchPoint
-      marker.markerElement.style.zIndex = zIndex++
+      marker.markerElement.style.zIndex = zIndex
+      zIndex += 1
       # Add `z-index` space for all the children of the marker.
       zIndex += marker.wrapper.numChildren if marker.wrapper.numChildren?
     return
@@ -106,7 +107,8 @@ injectHints = (window, wrappers, viewport, options) ->
   # unique `z-index` (space for this was added in `setZIndexes`).
   for marker in combined
     parent = markerMap[marker.wrapper.parentIndex]
-    marker.markerElement.style.zIndex = parent.markerElement.style.zIndex++
+    marker.markerElement.style.zIndex = parent.markerElement.style.zIndex
+    parent.markerElement.style.zIndex += 1
     marker.setHint(parent.hint)
   markers.push(combined...)
 

extension/lib/commands-frame.coffee
@@ -122,7 +122,7 @@ helper_follow = ({id, combine = true}, matcher, {vim}) ->
           parent = hrefs[href]
           wrapper.parentIndex = parent.elementIndex
           parent.shape.area += wrapper.shape.area
-          parent.numChildren++
+          parent.numChildren += 1
         else
           wrapper.numChildren = 0
           hrefs[href] = wrapper

extension/lib/events-frame.coffee
@@ -192,7 +192,7 @@ class FrameEventManager
 
       if @numFocusToSuppress > 0
         utils.suppressEvent(event)
-        @numFocusToSuppress--
+        @numFocusToSuppress -= 1
         return
 
       @vim.state.explicitBodyFocus = (target == @vim.content.document.body)

extension/lib/help.coffee
@@ -63,7 +63,7 @@ injectHelp = (window, vimfx) ->
   fontSize = originalFontSize =
     parseFloat(computedStyle.getPropertyValue('font-size'))
   while wrapper.scrollTopMax == 0 and fontSize <= MAX_FONT_SIZE
-    fontSize++
+    fontSize += 1
     container.style.fontSize = "#{fontSize}px"
   container.style.fontSize = "#{Math.max(fontSize - 1, originalFontSize)}px"
 

extension/lib/marker.coffee
@@ -104,13 +104,13 @@ class Marker
   matchHintChar: (char) ->
     if char == @hint[@hintIndex]
       @toggleLastHintChar(true)
-      @hintIndex++
+      @hintIndex += 1
       return true
     return false
 
   deleteHintChar: ->
     if @hintIndex > 0
-      @hintIndex--
+      @hintIndex -= 1
       @toggleLastHintChar(false)
 
   toggleLastHintChar: (visible) ->
@@ -176,7 +176,7 @@ getStackFor = (marker, markers) ->
       stack = stack.concat(getStackFor(nextMarker, markers))
     else
       # Continue the search.
-      index++
+      index += 1
 
   return stack
 

extension/lib/message-manager.coffee
@@ -63,7 +63,7 @@ send = (name, data = null, messageManager = defaultMM, callback = null) ->
   callbackName = null
   if callback
     callbackName = "#{name}:callback:#{callbackCounter}"
-    callbackCounter++
+    callbackCounter += 1
     listenOnce(callbackName, callback, messageManager)
 
   namespacedName = namespace(name)

extension/lib/modes.coffee
@@ -162,7 +162,7 @@ mode('hints', {
 
       if matchedMarkers.length > 0
         again = callback(matchedMarkers[0], storage.count, match.keyStr)
-        storage.count--
+        storage.count -= 1
         if again
           vim.window.setTimeout((->
             marker.markMatched(false) for marker in matchedMarkers
@@ -173,7 +173,7 @@ mode('hints', {
         else
           vim.enterMode('normal')
       else
-        storage.numEnteredChars++
+        storage.numEnteredChars += 1
 
     return true
 
@@ -195,9 +195,9 @@ mode('hints', {
       switch marker.hintIndex - storage.numEnteredChars
         when  0 then marker.deleteHintChar()
         when -1 then marker.show()
-    storage.numEnteredChars-- unless storage.numEnteredChars == 0
+    storage.numEnteredChars -= 1 unless storage.numEnteredChars == 0
 
-  increase_count: ({storage}) -> storage.count++
+  increase_count: ({storage}) -> storage.count += 1
 })
 
 
@@ -219,7 +219,7 @@ mode('ignore', {
       when 1
         vim.enterMode('normal')
       else
-        storage.count--
+        storage.count += 1
     return false
 
 }, {

extension/test/index.coffee
@@ -34,13 +34,13 @@ module.exports = (vimfx) ->
     tests = require("./#{name}")
     report.push(name)
     for key, fn of tests when key.startsWith('test')
-      total++
+      total += 1
       error = null
       teardowns = []
       teardown = (fn) -> teardowns.push(fn)
       try
         fn(assert, vimfx, teardown)
-        passed++
+        passed += 1
       catch error then null
       finally
         (try fn()) for fn in teardowns

commit 82db663729437b8fae9809893e3079972a49fc93

Fix a `z-index` bug for markers with the same hint
Regression since commit 1176cde8.

- `element.style.zIndex`: Always string.
- `element.style.zIndex = value`: Turns `value` into a string.
- `a = '5'; a++`: `a` is now `6`.
- `element.style.zIndex++`: If the previous value was `'5'`, the new value is
  `'6'`. This method was used before above commit.
- `element.style.zIndex += 1`: If the previous value was `'5'`, the new value is
  `'51'`. This was introduced by above commit.

The above really shows that you should always be explicit about types. Since
`element.style.zIndex` is always a string, `element.style.zIndex++` was a
terrible hack that happened to work, but caused a bug when it was rewritten to
a seemingly equivalent expression.

This commit first reads the `z-index` of the element and explicitly makes it a
`Number`, allowing incrementing to work as expected.

extension/lib/hints.coffee
@@ -110,8 +110,9 @@ injectHints = (window, wrappers, viewport, options) ->
   # unique `z-index` (space for this was added in `setZIndexes`).
   for marker in combined
     parent = markerMap[marker.wrapper.parentIndex]
-    marker.markerElement.style.zIndex = parent.markerElement.style.zIndex
-    parent.markerElement.style.zIndex += 1
+    parentZIndex = Number(parent.markerElement.style.zIndex)
+    marker.markerElement.style.zIndex = parentZIndex
+    parent.markerElement.style.zIndex = parentZIndex + 1
     marker.setHint(parent.hint)
   markers.push(combined...)
 