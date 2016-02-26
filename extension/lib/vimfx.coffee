akhodakivskiy/VimFx

commit da8b3ccbf4058e52a8b19f857e65636f67cbb9ea

Save the focusType instead of getting it on every keydown
This is simpler and more efficient. Also, we were basically already storing the
current focusType, since the button uses it to grey out when a text is focused.

Config API changes:

- `match` objects no longer have a `focus` property. It has been replaced with
  the `likelyConflict` property. (Theoretically a breaking change.)
- `vim.focusType` is now available and contains the current focus type.
- The `focusTypeChange` event now passes only the current `vim` object to
  listeners, instead of an object of data. Use `vim.focusType` to get the new
  focusType. (Theoretically a breaking change.)

extension/lib/vimfx.coffee
@@ -79,7 +79,7 @@ class VimFx extends EventEmitter
       translations: @options.translations
     })
 
-  consumeKeyEvent: (event, vim, focusType) ->
+  consumeKeyEvent: (event, vim) ->
     {mode} = vim
     return unless keyStr = @stringifyKeyEvent(event)
 
@@ -118,26 +118,24 @@ class VimFx extends EventEmitter
         @reset(mode)
 
     count = if @count == '' then undefined else Number(@count)
-    focus = @adjustFocusType(event, vim, focusType, keyStr)
     unmodifiedKey = notation.parse(keyStr).key
+
+    focusTypeKeys = @options["#{vim.focusType}_element_keys"]
+    likelyConflict =
+      if toplevel
+        if focusTypeKeys and keyStr in focusTypeKeys
+          true
+        else
+          vim.focusType != 'none'
+      else
+        false
+
     @reset(mode) if type == 'full'
     return {
-      type, focus, command, count, specialKeys, keyStr, unmodifiedKey, toplevel
+      type, command, count, specialKeys, keyStr, unmodifiedKey, toplevel
+      likelyConflict
       discard: @reset.bind(this, mode)
     }
-
-  adjustFocusType: (event, vim, focusType, keyStr) ->
-    # Frame scripts and the tests don’t pass in `originalTarget`.
-    document = event.originalTarget?.ownerDocument
-    if focusType == null and document and
-       # TODO: Remove when Tab Groups have been removed.
-       vim.window.TabView?.isVisible()
-      return 'other'
-
-    keys = @options["#{focusType}_element_keys"]
-    return null if keys and keyStr not in keys
-
-    return focusType
 
   getGroupedCommands: (options = {}) ->
     modes = {}

extension/lib/events-frame.coffee
@@ -122,7 +122,7 @@ class FrameEventManager
     @listen('keydown', (event) =>
       @keepInputs = false
 
-      suppress = @vim.onInput(event)
+      suppress = messageManager.get('consumeKeyEvent')
 
       # This also suppresses the 'keypress' and 'keyup' events. (Yes, in frame
       # scripts, suppressing the 'keydown' events does seem to even suppress

extension/lib/events.coffee
@@ -96,13 +96,12 @@ class UIEventManager
       return unless vim = @vimfx.getCurrentVim(@window)
 
       if vim.isUIEvent(event)
-        focusType = utils.getFocusType(event.originalTarget)
-        @consumeKeyEvent(vim, event, focusType, event)
+        @consumeKeyEvent(vim, event, event)
         # This also suppresses the 'keypress' event.
         utils.suppressEvent(event) if @suppress
       else
-        vim._listenOnce('consumeKeyEvent', ({focusType}) =>
-          @consumeKeyEvent(vim, event, focusType)
+        vim._listenOnce('consumeKeyEvent', =>
+          @consumeKeyEvent(vim, event, false)
           return @suppress
         )
     )
@@ -205,12 +204,12 @@ class UIEventManager
       callback(true)
     ), {messageManager: @window.messageManager})
 
-  consumeKeyEvent: (vim, event, focusType, uiEvent = false) ->
-    match = vim._consumeKeyEvent(event, focusType)
+  consumeKeyEvent: (vim, event, uiEvent = false) ->
+    match = vim._consumeKeyEvent(event)
 
     if match
       if @vimfx.options.notify_entered_keys
-        if match.type in ['none', 'full'] or match.focus != null
+        if match.type in ['none', 'full'] or match.likelyConflict
           @enteredKeys.clear(vim)
         else
           @enteredKeys.push(vim, match.keyStr, @vimfx.options.timeout)

extension/lib/main.coffee
@@ -65,7 +65,7 @@ module.exports = (data, reason) ->
 
   button.injectButton(vimfx)
 
-  setWindowAttribute = (window, name, value = 'none') ->
+  setWindowAttribute = (window, name, value) ->
     window.document.documentElement.setAttribute("vimfx-#{name}", value)
 
   onModeDisplayChange = (vimOrEvent) ->
@@ -81,8 +81,8 @@ module.exports = (data, reason) ->
   vimfx.on('modeChange', onModeDisplayChange)
   vimfx.on('TabSelect',  onModeDisplayChange)
 
-  vimfx.on('focusTypeChange', ({vim, focusType}) ->
-    setWindowAttribute(vim.window, 'focus-type', focusType)
+  vimfx.on('focusTypeChange', (vim) ->
+    setWindowAttribute(vim.window, 'focus-type', vim.focusType)
   )
 
   windows = new WeakSet()
@@ -101,7 +101,7 @@ module.exports = (data, reason) ->
       eventManager = new UIEventManager(vimfx, window)
       eventManager.addListeners(vimfx, window)
       setWindowAttribute(window, 'mode', 'normal')
-      setWindowAttribute(window, 'focus-type', null)
+      setWindowAttribute(window, 'focus-type', 'none')
 
     callback(true)
   )

extension/lib/modes.coffee
@@ -63,9 +63,8 @@ mode('normal', {
     {vim, storage, uiEvent} = args
     {keyStr} = match
 
-    autoInsertMode = (match.focus != null)
     if match.type == 'none' or
-       (autoInsertMode and not match.specialKeys['<force>'])
+       (match.likelyConflict and not match.specialKeys['<force>'])
       match.discard()
       if storage.returnTo
         vim.enterMode(storage.returnTo)
@@ -98,10 +97,10 @@ mode('normal', {
       # open the split console.
       return uiEvent.originalTarget.ownerGlobal.DevTools?
     else
-      # In web pages content, an exception is made if we’re in autoInsertMode.
-      # That allows for blurring an input in a custom dialog without closing the
-      # dialog too.
-      return autoInsertMode
+      # In web pages content, an exception is made if an element that VimFx
+      # cares about is focused. That allows for blurring an input in a custom
+      # dialog without closing the dialog too.
+      return vim.focusType != 'none'
 
     # Note that this special handling of Escape is only used in Normal mode.
     # There are two reasons we might suppress it in other modes. If some custom

extension/lib/utils.coffee
@@ -201,8 +201,11 @@ getFocusType = (element) -> switch
     'activatable'
   when isAdjustable(element)
     'adjustable'
+  # TODO: Remove when Tab Groups have been removed.
+  when element.ownerGlobal.TabView?.isVisible?()
+    'other'
   else
-    null
+    'none'
 
 
 

extension/lib/vim-frame.coffee
@@ -82,11 +82,6 @@ class VimFrame
       args: [@mode, args...]
     })
 
-  onInput: (event) ->
-    focusType = utils.getFocusType(event.originalTarget)
-    suppress = messageManager.get('consumeKeyEvent', {focusType})
-    return suppress
-
   notify: (args...) ->
     messageManager.send('vimMethod', {method: 'notify', args})
 

extension/lib/vim.coffee
@@ -33,6 +33,7 @@ ChromeWindow = Ci.nsIDOMChromeWindow
 
 class Vim
   constructor: (browser, @_parent) ->
+    @focusType = 'none'
     @_setBrowser(browser, {addListeners: false})
     @_storage = {}
 
@@ -126,8 +127,8 @@ class Vim
     @_send('modeChange', {mode})
     return result
 
-  _consumeKeyEvent: (event, focusType) ->
-    return @_parent.consumeKeyEvent(event, this, focusType)
+  _consumeKeyEvent: (event) ->
+    return @_parent.consumeKeyEvent(event, this)
 
   _onInput: (match, uiEvent = false) ->
     suppress = @_call('onInput', {uiEvent, count: match.count}, match)
@@ -182,12 +183,12 @@ class Vim
     @browser.focus()
     @_run('focus_marker_element', {elementIndex, options})
 
-  _setFocusType: (focusType) ->
+  _setFocusType: (@focusType) ->
     switch
-      when focusType == 'ignore'
+      when @focusType == 'ignore'
         @enterMode('ignore', {type: 'focusType'})
       when @mode == 'ignore' and @_storage.ignore.type == 'focusType'
         @enterMode('normal')
-    @_parent.emit('focusTypeChange', {vim: this, focusType})
+    @_parent.emit('focusTypeChange', this)
 
 module.exports = Vim

commit 67aa788bc57d81c8aeb2d3cfea0e465059ffdff2

Fix activatable and adjustable keys handling
Regression since commit da8b3ccb.

extension/lib/vimfx.coffee
@@ -123,8 +123,8 @@ class VimFx extends EventEmitter
     focusTypeKeys = @options["#{vim.focusType}_element_keys"]
     likelyConflict =
       if toplevel
-        if focusTypeKeys and keyStr in focusTypeKeys
-          true
+        if focusTypeKeys
+          keyStr in focusTypeKeys
         else
           vim.focusType != 'none'
       else