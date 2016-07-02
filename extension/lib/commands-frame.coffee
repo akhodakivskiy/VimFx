akhodakivskiy/VimFx

commit 1cf724c57a8df045c3e30504a37ba488277ca054

Simplify detection of markable `<label>`s
Apparently, such elements have a `.control` property that returns their
associated form control. No need to manually look for the 'for'
attribute or nested form control elements.

extension/lib/commands-frame.coffee
@@ -258,14 +258,9 @@ commands.follow = helper_follow.bind(
       # but keeps the `<label>` to click, either for styling purposes or to keep
       # the `<input>` hidden until it is used. In those cases we should add a
       # marker for the `<label>`.
-      when element.localName == 'label'
-        input =
-          if element.htmlFor
-            document.getElementById?(element.htmlFor)
-          else
-            element.querySelector?('input, textarea, select')
-        if input and not getElementShape(input).nonCoveredPoint
-          type = 'clickable'
+      when element.localName == 'label' and element.control and
+           not getElementShape(element.control).nonCoveredPoint
+        type = 'clickable'
       # Last resort checks for elements that might be clickable because of
       # JavaScript.
       when (not isXUL and

commit ec80795f5ef784fdfe8082b97327ce4073d86550

Prevent `f` commands from crashing in about:config
Regression since commit 1cf724c5.

extension/lib/commands-frame.coffee
@@ -257,8 +257,9 @@ commands.follow = helper_follow.bind(
       # its `<input>` gets one. However, some sites hide the actual `<input>`
       # but keeps the `<label>` to click, either for styling purposes or to keep
       # the `<input>` hidden until it is used. In those cases we should add a
-      # marker for the `<label>`.
-      when element.localName == 'label' and element.control and
+      # marker for the `<label>`. Trying to access `.control` on an element in
+      # `about:config` throws an error, so exclude XUL pages.
+      when not isXUL and element.localName == 'label' and element.control and
            not getElementShape(element.control).nonCoveredPoint
         type = 'clickable'
       # Last resort checks for elements that might be clickable because of