akhodakivskiy/VimFx

commit c4c417368af62e8b27f20121554f4ea03ef81c6e

Make it possible to create custom hint commands
My first idea was to use `vim.enterMode('hints', ...)`. However, that's
too complicated to be useful for any user.

Actually, I found that `vim.enterMode(...)` is too complicated for most
(all?) modes, so I decided to make it private (removing it from the
public API) while at it. (I also made the arguments of
`vim.enterMode`/`mode.onEnter` more consistent with each other.)

I also considered exposing the entire "API" that the `f` commands use
internally. However, that's more complicated than the average user
needs, and would increase the API surface _a lot._

Instead, the `f` commands now take a `callbackOverride` argument. This
lets you choose the `f` command that matches the elements you're looking
for and the override what happens when a hint marker is matched.

Fixes #785. Fixes #654.

extension/lib/commands.coffee
@@ -241,11 +241,13 @@ helper_mark_last_scroll_position = (vim) ->
   vim._run('mark_scroll_position', {keyStr, notify: false})
 
 commands.mark_scroll_position = ({vim}) ->
-  vim.enterMode('marks', (keyStr) -> vim._run('mark_scroll_position', {keyStr}))
+  vim._enterMode('marks', (keyStr) ->
+    vim._run('mark_scroll_position', {keyStr})
+  )
   vim.notify(translate('notification.mark_scroll_position.enter'))
 
 commands.scroll_to_mark = ({vim}) ->
-  vim.enterMode('marks', (keyStr) ->
+  vim._enterMode('marks', (keyStr) ->
     unless keyStr == vim.options['scroll.last_position_mark']
       helper_mark_last_scroll_position(vim)
     helper_scroll(
@@ -428,7 +430,7 @@ commands.tab_close_other = ({vim}) ->
 
 
 
-helper_follow = (name, vim, callback, count = null) ->
+helper_follow = ({name, callback}, {vim, count, callbackOverride = null}) ->
   {window} = vim
   vim.markPageInteraction()
   help.removeHelp(window)
@@ -452,11 +454,19 @@ helper_follow = (name, vim, callback, count = null) ->
     markerContainer.container
   )
 
+  chooseCallback = (marker, timesLeft, keyStr) ->
+    if callbackOverride
+      {type, href = null, elementIndex} = marker.wrapper
+      return callbackOverride({type, href, id: elementIndex, timesLeft})
+    else
+      return callback(marker, timesLeft, keyStr)
+
   # Enter Hints mode immediately, with an empty set of markers. The user might
   # press keys before any hints have been generated. Those key presses should be
   # handled in Hints mode, not Normal mode.
-  vim.enterMode('hints', {
-    markerContainer, callback, count
+  vim._enterMode('hints', {
+    markerContainer, count
+    callback: chooseCallback
     sleep: vim.options.hints_sleep
   })
 
@@ -467,7 +477,7 @@ helper_follow = (name, vim, callback, count = null) ->
     if wrappers.length == 0
       if pass in ['single', 'second'] and markerContainer.markers.length == 0
         vim.notify(translate('notification.follow.none'))
-        vim.enterMode('normal')
+        vim._enterMode('normal')
     else
       markerContainer.injectHints(wrappers, viewport, pass)
 
@@ -476,7 +486,9 @@ helper_follow = (name, vim, callback, count = null) ->
 
   vim._run(name, {pass: 'auto'}, injectHints)
 
-helper_follow_clickable = (options, {vim, count = 1}) ->
+helper_follow_clickable = (options, args) ->
+  {vim} = args
+
   callback = (marker, timesLeft, keyStr) ->
     {inTab, inBackground} = options
     {type, elementIndex} = marker.wrapper
@@ -527,7 +539,7 @@ helper_follow_clickable = (options, {vim, count = 1}) ->
     return not isLast
 
   name = if options.inTab then 'follow_in_tab' else 'follow'
-  helper_follow(name, vim, callback, count)
+  helper_follow({name, callback}, args)
 
 commands.follow =
   helper_follow_clickable.bind(null, {inTab: false, inBackground: true})
@@ -538,19 +550,24 @@ commands.follow_in_tab =
 commands.follow_in_focused_tab =
   helper_follow_clickable.bind(null, {inTab: true, inBackground: false})
 
-commands.follow_in_window = ({vim}) ->
+commands.follow_in_window = (args) ->
+  {vim} = args
+
   callback = (marker) ->
     vim._focusMarkerElement(marker.wrapper.elementIndex)
     {href} = marker.wrapper
     vim.window.openLinkIn(href, 'window', {}) if href
     return false
-  helper_follow('follow_in_tab', vim, callback)
+
+  helper_follow({name: 'follow_in_tab', callback}, args)
 
 commands.follow_multiple = (args) ->
   args.count = Infinity
   commands.follow(args)
 
-commands.follow_copy = ({vim}) ->
+commands.follow_copy = (args) ->
+  {vim} = args
+
   callback = (marker) ->
     property = switch marker.wrapper.type
       when 'link'
@@ -561,13 +578,17 @@ commands.follow_copy = ({vim}) ->
         '_selection'
     helper_copy_marker_element(vim, marker.wrapper.elementIndex, property)
     return false
-  helper_follow('follow_copy', vim, callback)
 
-commands.follow_focus = ({vim}) ->
+  helper_follow({name: 'follow_copy', callback}, args)
+
+commands.follow_focus = (args) ->
+  {vim} = args
+
   callback = (marker) ->
     vim._focusMarkerElement(marker.wrapper.elementIndex, {select: true})
     return false
-  helper_follow('follow_focus', vim, callback)
+
+  helper_follow({name: 'follow_focus', callback}, args)
 
 commands.click_browser_element = ({vim}) ->
   {window} = vim
@@ -663,7 +684,7 @@ commands.click_browser_element = ({vim}) ->
     )
 
     markerContainer.injectHints(wrappers, viewport, 'single')
-    vim.enterMode('hints', {markerContainer, callback})
+    vim._enterMode('hints', {markerContainer, callback})
 
   else
     vim.notify(translate('notification.follow.none'))
@@ -684,16 +705,19 @@ commands.focus_text_input = ({vim, count}) ->
   vim.markPageInteraction()
   vim._run('focus_text_input', {count})
 
-helper_follow_selectable = ({select}, {vim}) ->
+helper_follow_selectable = ({select}, args) ->
+  {vim} = args
+
   callback = (marker) ->
     vim._run('element_text_select', {
       elementIndex: marker.wrapper.elementIndex
       full: select
       scroll: select
     })
-    vim.enterMode('caret', select)
+    vim._enterMode('caret', {select})
     return false
-  helper_follow('follow_selectable', vim, callback)
+
+  helper_follow({name: 'follow_selectable', callback}, args)
 
 commands.element_text_caret =
   helper_follow_selectable.bind(null, {select: false})
@@ -701,11 +725,12 @@ commands.element_text_caret =
 commands.element_text_select =
   helper_follow_selectable.bind(null, {select: true})
 
-commands.element_text_copy = ({vim}) ->
+commands.element_text_copy = (args) ->
+  {vim} = args
   callback = (marker) ->
     helper_copy_marker_element(vim, marker.wrapper.elementIndex, '_selection')
     return false
-  helper_follow('follow_selectable', vim, callback)
+  helper_follow(args, {name: 'follow_selectable', callback})
 
 helper_copy_marker_element = (vim, elementIndex, property) ->
   if property == '_selection'
@@ -747,7 +772,7 @@ helper_find = ({highlight, linksOnly = false}, {vim}) ->
 
   # In case `helper_find_from_top_of_viewport` is slow, make sure that keys
   # pressed before the find bar input is focsued doesn’t trigger commands.
-  vim.enterMode('find')
+  vim._enterMode('find')
 
   helper_mark_last_scroll_position(vim)
   helper_find_from_top_of_viewport(vim, FORWARD, ->
@@ -806,12 +831,13 @@ commands.window_new = ({vim}) ->
 commands.window_new_private = ({vim}) ->
   vim.window.OpenBrowserWindow({private: true})
 
-commands.enter_mode_ignore = ({vim}) ->
-  vim.enterMode('ignore', {type: 'explicit'})
+commands.enter_mode_ignore = ({vim, blacklist = false}) ->
+  type = if blacklist then 'blacklist' else 'explicit'
+  vim._enterMode('ignore', {type})
 
 # Quote next keypress (pass it through to the page).
 commands.quote = ({vim, count = 1}) ->
-  vim.enterMode('ignore', {type: 'explicit', count})
+  vim._enterMode('ignore', {type: 'explicit', count})
 
 commands.enter_reader_view = ({vim}) ->
   button = vim.window.document.getElementById('reader-mode-button')

extension/lib/api-frame.coffee
@@ -44,6 +44,10 @@ createConfigAPI = (vim, onShutdown = module.onShutdown) -> {
       )
     vim.hintMatcher = hintMatcher
     onShutdown(-> vim.hintMatcher = null)
+
+  getMarkerElement: (id) ->
+    data = vim.state.markerElements[id]
+    return if data then data.element else null
 }
 
 module.exports = createConfigAPI

extension/lib/button.coffee
@@ -47,7 +47,7 @@ injectButton = (vimfx) ->
       if vim.mode == 'normal' and not helpVisible
         help.injectHelp(window, vimfx)
       else
-        vim.enterMode('normal')
+        vim._enterMode('normal')
   })
   module.onShutdown(-> cui.destroyWidget(BUTTON_ID))
 

extension/lib/events-frame.coffee
@@ -95,7 +95,7 @@ class FrameEventManager
 
       if target == @vim.content.document
         messageManager.send('frameCanReceiveEvents', false)
-        @vim.enterMode('normal') if @vim.mode == 'hints'
+        @vim._enterMode('normal') if @vim.mode == 'hints'
 
       # If the target isn’t the topmost document, it means that a frame has
       # changed: It could have been removed or its `src` attribute could have
@@ -224,7 +224,7 @@ class FrameEventManager
         @vim.state.hasFocusedTextInput = true
 
         if @vim.mode == 'caret' and not utils.isContentEditable(target)
-          @vim.enterMode('normal')
+          @vim._enterMode('normal')
 
       # Blur the focus target, if autofocus prevention is enabled…
       if prefs.get('prevent_autofocus') and

extension/lib/events.coffee
@@ -130,7 +130,7 @@ class UIEventManager
          # mode. Otherwise we’d first return to normal mode and then the button
          # would open the help dialog.
          target != button.getButton(@window)
-        vim.enterMode('normal')
+        vim._enterMode('normal')
 
       vim._send('clearHover') unless isVimFxGeneratedEvent
     )
@@ -201,7 +201,7 @@ class UIEventManager
     vim._setFocusType(focusType)
 
     if focusType == 'editable' and vim.mode == 'caret'
-      vim.enterMode('normal')
+      vim._enterMode('normal')
 
   consumeKeyEvent: (vim, event) ->
     match = vim._consumeKeyEvent(event)

extension/lib/modes.coffee
@@ -52,11 +52,11 @@ mode = (modeName, obj, commands = null) ->
 
 
 mode('normal', {
-  onEnter: ({vim, storage}, options = {}) ->
-    if options.returnTo
-      storage.returnTo = options.returnTo
+  onEnter: ({vim, storage}, {returnTo = null} = {}) ->
+    if returnTo
+      storage.returnTo = returnTo
     else if storage.returnTo
-      vim.enterMode(storage.returnTo)
+      vim._enterMode(storage.returnTo)
       storage.returnTo = null
 
   onLeave: ({vim}) ->
@@ -70,7 +70,7 @@ mode('normal', {
        (match.likelyConflict and not match.specialKeys['<force>'])
       match.discard()
       if storage.returnTo
-        vim.enterMode(storage.returnTo)
+        vim._enterMode(storage.returnTo)
         storage.returnTo = null
       # If you press `aa` (and `a` is a prefix key, but there’s no `aa`
       # shortcut), don’t pass the second `a` to the page.
@@ -82,7 +82,7 @@ mode('normal', {
       # If the command changed the mode, wait until coming back from that mode
       # before switching to `storage.returnTo` if any (see `onEnter` above).
       if storage.returnTo and vim.mode == 'normal'
-        vim.enterMode(storage.returnTo)
+        vim._enterMode(storage.returnTo)
         storage.returnTo = null
 
     # At this point the match is either full, partial or part of a count. Then
@@ -122,7 +122,7 @@ helper_move_caret = (method, direction, {vim, storage, count = 1}) ->
   })
 
 mode('caret', {
-  onEnter: ({vim, storage}, select = false) ->
+  onEnter: ({vim, storage}, {select = false} = {}) ->
     storage.select = select
     storage.caretBrowsingPref = prefs.root.get(CARET_BROWSING_PREF)
     prefs.root.set(CARET_BROWSING_PREF, true)
@@ -182,11 +182,11 @@ mode('caret', {
         # clipboard, since `window.getSelection().toString()` sadly collapses
         # whitespace in `<pre>` elements.
         vim.window.goDoCommand('cmd_copy')
-        vim.enterMode('normal')
+        vim._enterMode('normal')
     )
 
   exit: ({vim}) ->
-    vim.enterMode('normal')
+    vim._enterMode('normal')
 })
 
 
@@ -241,7 +241,7 @@ mode('hints', {
         else
           # The callback might have entered another mode. Only go back to Normal
           # mode if we’re still in Hints mode.
-          vim.enterMode('normal') if vim.mode == 'hints'
+          vim._enterMode('normal') if vim.mode == 'hints'
 
     return true
 
@@ -250,7 +250,7 @@ mode('hints', {
     # The hints are removed automatically when leaving the mode, but after a
     # timeout. When aborting the mode we should remove the hints immediately.
     storage.markerContainer.remove()
-    vim.enterMode('normal')
+    vim._enterMode('normal')
 
   rotate_markers_forward: ({storage}) ->
     storage.markerContainer.rotateOverlapping(true)
@@ -294,7 +294,7 @@ mode('ignore', {
           match.command.run(args)
           return true
       when 1
-        vim.enterMode('normal')
+        vim._enterMode('normal')
       else
         storage.count -= 1
     return false
@@ -302,9 +302,9 @@ mode('ignore', {
 }, {
   exit: ({vim, storage}) ->
     storage.type = null
-    vim.enterMode('normal')
+    vim._enterMode('normal')
   unquote: ({vim}) ->
-    vim.enterMode('normal', {returnTo: 'ignore'})
+    vim._enterMode('normal', {returnTo: 'ignore'})
 })
 
 
@@ -325,7 +325,7 @@ mode('find', {
 
 }, {
   exit: ({vim, findBar}) ->
-    vim.enterMode('normal')
+    vim._enterMode('normal')
     findBar.close()
 })
 
@@ -336,7 +336,7 @@ mode('marks', {
     storage.callback = callback
     storage.timeoutId = vim.window.setTimeout((->
       vim.hideNotification()
-      vim.enterMode('normal')
+      vim._enterMode('normal')
     ), vim.options.timeout)
 
   onLeave: ({vim, storage}) ->
@@ -350,9 +350,9 @@ mode('marks', {
       match.command.run(args)
     else
       storage.callback(match.keyStr)
-      vim.enterMode('normal')
+      vim._enterMode('normal')
     return true
 }, {
   exit: ({vim}) ->
-    vim.enterMode('normal')
+    vim._enterMode('normal')
 })

extension/lib/vim-frame.coffee
@@ -75,9 +75,9 @@ class VimFrame
       # `markerElements` and `inputs` could theoretically need to be filtered
       # too at this point. YAGNI until an issue arises from it.
 
-  enterMode: (@mode, args...) ->
+  _enterMode: (@mode, args...) ->
     messageManager.send('vimMethod', {
-      method: 'enterMode'
+      method: '_enterMode'
       args: [@mode, args...]
     })
 

extension/lib/vim.coffee
@@ -104,7 +104,7 @@ class Vim
            element != @window.gBrowser.selectedBrowser
 
   # `args...` is passed to the mode's `onEnter` method.
-  enterMode: (mode, args...) ->
+  _enterMode: (mode, args...) ->
     return if @mode == mode
 
     unless utils.has(@_parent.modes, mode)
@@ -115,10 +115,9 @@ class Vim
 
     @_call('onLeave') if @mode?
     @mode = mode
-    result = @_call('onEnter', null, args...)
+    @_call('onEnter', null, args...)
     @_parent.emit('modeChange', {vim: this})
     @_send('modeChange', {mode})
-    return result
 
   _consumeKeyEvent: (event) ->
     return @_parent.consumeKeyEvent(event, this)
@@ -131,10 +130,10 @@ class Vim
   _onLocationChange: (url) ->
     switch
       when @_isBlacklisted(url)
-        @enterMode('ignore', {type: 'blacklist'})
+        @_enterMode('ignore', {type: 'blacklist'})
       when (@mode == 'ignore' and @_storage.ignore.type == 'blacklist') or
            not @mode
-        @enterMode('normal')
+        @_enterMode('normal')
     @_parent.emit('locationChange', {vim: this, location: new @window.URL(url)})
 
   _call: (method, data = {}, extraArgs...) ->
@@ -183,13 +182,13 @@ class Vim
     @focusType = focusType
     switch
       when @focusType == 'ignore'
-        @enterMode('ignore', {type: 'focusType'})
+        @_enterMode('ignore', {type: 'focusType'})
       when @mode == 'ignore' and @_storage.ignore.type == 'focusType'
-        @enterMode('normal')
+        @_enterMode('normal')
       when @mode == 'normal' and @focusType == 'findbar'
-        @enterMode('find')
+        @_enterMode('find')
       when @mode == 'find' and @focusType != 'findbar'
-        @enterMode('normal')
+        @_enterMode('normal')
     @_parent.emit('focusTypeChange', {vim: this})
 
 module.exports = Vim

commit d7b6d41546e82641beec9719a004aa69eb69e47b

Fix broken `yv` command
Regression since commit c4c41736.

extension/lib/commands.coffee
@@ -727,10 +727,12 @@ commands.element_text_select =
 
 commands.element_text_copy = (args) ->
   {vim} = args
+
   callback = (marker) ->
     helper_copy_marker_element(vim, marker.wrapper.elementIndex, '_selection')
     return false
-  helper_follow(args, {name: 'follow_selectable', callback})
+
+  helper_follow({name: 'follow_selectable', callback}, args)
 
 helper_copy_marker_element = (vim, elementIndex, property) ->
   if property == '_selection'