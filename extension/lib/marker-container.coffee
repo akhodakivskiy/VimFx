akhodakivskiy/VimFx

commit e70ab84982d4a000841ca6ba8e0b803ca65726a9

Fix highlighting of hint markers in the second pass

extension/lib/marker-container.coffee
@@ -190,6 +190,8 @@ class MarkerContainer
     # than newer markers, which is why we start out large and not at zero.
     zIndex = MAX_Z_INDEX - markers.length - @markers.length + 1
     markers.sort((a, b) -> a.wrapper.shape.area - b.wrapper.shape.area)
+
+    @resetHighlightedMarkers()
     for marker in markers
       marker.markerElement.style.zIndex = zIndex
       zIndex += 1
@@ -199,7 +201,6 @@ class MarkerContainer
         marker.setHint(parent.hint)
 
       @updateHighlightedMarkers(marker)
-
     @markHighlightedMarkers()
 
     fragment = @window.document.createDocumentFragment()

commit eab413445f0de136187f52c07adc98255858b020

Don't reset highlighted hint markers in the second pass
When injecting hint markers for the second pass, do reset which hint
markers are visually highlighted (in case they change), but _don't_
reset the actual data. Regression since commit e70ab849.

extension/lib/marker-container.coffee
@@ -87,9 +87,9 @@ class MarkerContainer
         marker.hide()
     @markHighlightedMarkers()
 
-  resetHighlightedMarkers: ->
+  resetHighlightedMarkers: ({visuallyOnly = false} = {}) ->
     marker.markHighlighted(false) for marker in @highlightedMarkers
-    @highlightedMarkers = []
+    @highlightedMarkers = [] unless visuallyOnly
 
   markHighlightedMarkers: ->
     marker.markHighlighted(true) for marker in @highlightedMarkers
@@ -191,7 +191,7 @@ class MarkerContainer
     zIndex = MAX_Z_INDEX - markers.length - @markers.length + 1
     markers.sort((a, b) -> a.wrapper.shape.area - b.wrapper.shape.area)
 
-    @resetHighlightedMarkers()
+    @resetHighlightedMarkers({visuallyOnly: true})
     for marker in markers
       marker.markerElement.style.zIndex = zIndex
       zIndex += 1