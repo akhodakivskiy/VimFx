akhodakivskiy/VimFx

commit d0d04fc44572635d5b9302a90cf219f52b618841

Improve holding keys for scrolling by page
Fixes #843.

extension/lib/commands.coffee
@@ -147,9 +147,10 @@ commands.stop_all = ({vim}) ->
 
 
 
-springConstant = {
+scrollData = {
   nonce: null
-  value: null
+  springConstant: null
+  lastRepeat: 0
 }
 
 helper_scroll = (vim, event, args...) ->
@@ -157,6 +158,14 @@ helper_scroll = (vim, event, args...) ->
     method, type, directions, amounts
     properties = null, adjustment = 0, name = 'scroll', extra = {}
   ] = args
+
+  elapsed = event.timeStamp - scrollData.lastRepeat
+
+  if event.repeat and elapsed < vim.options['scroll.repeat_timeout']
+    return
+
+  scrollData.lastRepeat = event.timeStamp
+
   options = {
     method, type, directions, amounts, properties, adjustment, extra
     smooth: (
@@ -168,18 +177,18 @@ helper_scroll = (vim, event, args...) ->
   # Temporarily set Firefox’s “spring constant” pref to get the desired smooth
   # scrolling speed. Reset it `reset_timeout` milliseconds after the last
   # scrolling command was invoked.
-  springConstant.nonce = nonce = {}
-  springConstant.value ?= prefs.root.get(SPRING_CONSTANT_PREF)
+  scrollData.nonce = nonce = {}
+  scrollData.springConstant ?= prefs.root.get(SPRING_CONSTANT_PREF)
   prefs.root.set(
     SPRING_CONSTANT_PREF,
     vim.options["smoothScroll.#{type}.spring-constant"]
   )
   reset = ->
     vim.window.setTimeout((->
-      return unless springConstant.nonce == nonce
-      prefs.root.set(SPRING_CONSTANT_PREF, springConstant.value)
-      springConstant.nonce = null
-      springConstant.value = null
+      return unless scrollData.nonce == nonce
+      prefs.root.set(SPRING_CONSTANT_PREF, scrollData.springConstant)
+      scrollData.nonce = null
+      scrollData.springConstant = null
     ), vim.options['scroll.reset_timeout'])
 
   helpScroll = help.getHelp(vim.window)?.querySelector('.wrapper')

extension/lib/defaults.coffee
@@ -186,6 +186,7 @@ advanced_options =
   'smoothScroll.pages.spring-constant': '2500'
   'smoothScroll.other.spring-constant': '2500'
   'scroll.reset_timeout':               1000
+  'scroll.repeat_timeout':              65
   'scroll.horizontal_boost':            6
   'scroll.vertical_boost':              3
   'scroll.full_page_adjustment':        40

commit 783e21d7839d1e18fb25aa1910fbfcbd5301a3ad

Fix the `'`, `g[` and `g]` commands crashing
Regression since commit d0d04fc44.

Fixes #848.

extension/lib/commands.coffee
@@ -191,8 +191,9 @@ helper_scroll = (vim, event, args...) ->
       scrollData.springConstant = null
     ), vim.options['scroll.reset_timeout'])
 
+  {isUIEvent = vim.isUIEvent(event)} = extra
   helpScroll = help.getHelp(vim.window)?.querySelector('.wrapper')
-  if (event and vim.isUIEvent(event)) or helpScroll
+  if isUIEvent or helpScroll
     activeElement = helpScroll or utils.getActiveElement(vim.window)
     if vim._state.scrollableElements.has(activeElement) or helpScroll
       viewportUtils.scroll(activeElement, options)
@@ -260,23 +261,24 @@ commands.mark_scroll_position = ({vim}) ->
   )
   vim.notify(translate('notification.mark_scroll_position.enter'))
 
-commands.scroll_to_mark = ({vim}) ->
+commands.scroll_to_mark = ({vim, event}) ->
   vim._enterMode('marks', (keyStr) ->
+    lastPositionMark = vim.options['scroll.last_position_mark']
     helper_scroll(
-      vim, null, 'scrollTo', 'other', ['left', 'top'], [0, 0]
+      vim, event, 'scrollTo', 'other', ['left', 'top'], [0, 0]
       ['scrollLeftMax', 'scrollTopMax'], 0, 'scroll_to_mark'
-      {keyStr, lastPositionMark: vim.options['scroll.last_position_mark']}
+      {keyStr, lastPositionMark, isUIEvent: false}
     )
     vim.hideNotification()
   )
   vim.notify(translate('notification.scroll_to_mark.enter'))
 
-helper_scroll_to_position = (direction, {vim, count = 1}) ->
+helper_scroll_to_position = (direction, {vim, event, count = 1}) ->
   lastPositionMark = vim.options['scroll.last_position_mark']
   helper_scroll(
-    vim, null, 'scrollTo', 'other', ['left', 'top'], [0, 0]
+    vim, event, 'scrollTo', 'other', ['left', 'top'], [0, 0]
     ['scrollLeftMax', 'scrollTopMax'], 0, 'scroll_to_position'
-    {count, direction, lastPositionMark}
+    {count, direction, lastPositionMark, isUIEvent: false}
   )
 
 commands.scroll_to_previous_position =